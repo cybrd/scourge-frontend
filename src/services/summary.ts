ff6d5ce0c6b2162ccc75d3751ba08bc3bc1516d6
cybrd <brysuralta@gmail.com>
2024-11-01 06:12:32 +0800

    add component summary 3

--- src/services/summary.ts
+++ src/services/summary.ts
@@ -1,10 +1,14 @@
 import { StatusCodes } from "http-status-codes";
 
 import { MemberWithPoints } from "../models/member";
+import { Query } from "../models/query";
 
-export const summary = () =>
+export const summary = (options: Query) =>
   fetch(`${import.meta.env.VITE_API_SERVER}/summary`, {
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      Authorization: `Bearer ${options.token}`,
+      "Content-Type": "application/json",
+    },
   }).then((res): Promise<MemberWithPoints[]> => {
     if (res.status !== StatusCodes.OK) {
       throw StatusCodes[res.status];
